680e0ab56e789200389f5118bd0f17a7f211b2ba
wpallen12 <wallen@spauldingridge.com>
2024-07-29 15:40:54 -0500

    Update streamlit_ap.py
    Copied from my custom smoothies SiS app

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-
+from snowflake.snowpark.functions import col
 # Write directly to the app
 st.title("Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -9,11 +8,11 @@ st.write(
     """
 )
 
-from snowflake.snowpark.functions import col
 
 
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
