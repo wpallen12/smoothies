cfa39e172fa1a869bdbe1757a8c19171b6e2eca1
wpallen12 <allenwp375@gmail.com>
2024-07-30 09:18:12 -0500

    Update streamlit_ap.py

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -37,7 +37,8 @@ if ingredients_list:
     #add a for loop to go through each row in the ingredients list
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '#the += simply adds to what is already in the variable, so each time the loop runs a new fruit will be added to the string
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     
     #st.write(ingredients_string)
