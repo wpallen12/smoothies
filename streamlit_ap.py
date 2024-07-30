8410ac46d59ba35ba6d6b57db22628d6127be674
wpallen12 <allenwp375@gmail.com>
2024-07-30 08:42:42 -0500

    Update streamlit_ap.py
    Updating

--- streamlit_ap.py
+++ streamlit_ap.py
@@ -48,4 +48,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered!', icon="âœ…")
+        st.success('Your Smoothie is ordered!')
